62de0024e77f2177c21f4ecb6831e36cca4cdd23
Derek Fredrickson <dfreds08@gmail.com>
2025-03-11 23:28:35 -0400

    Remove editUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/DFreds/dfreds-module-docs/tree/main',
+          // editUrl: 'https://github.com/DFreds/dfreds-module-docs/tree/main',
           sidebarCollapsible: true,
           sidebarCollapsed: false,
         },
